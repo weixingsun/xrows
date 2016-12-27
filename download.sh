f830b18d4481bd11caab95e5a49d7fc20188eabf
Weixing Sun <weixing.sun@gmail.com>
2016-12-28 01:04:41 +1300

    fix release ios bug: no script url provided

--- download.sh
+++ download.sh
@@ -1,2 +1,3 @@
 curl -o index.android.bundle http://localhost:8081/index.android.bundle?platform=android&dev=true&hot=false&minify=false
-curl -o index.ios.jsbundle   http://localhost:8081/index.ios.bundle?platform=ios&dev=true
+#curl -o index.ios.jsbundle   http://localhost:8081/index.ios.bundle?platform=ios&dev=true
+curl -o main.jsbundle   http://localhost:8081/index.ios.bundle?platform=ios
